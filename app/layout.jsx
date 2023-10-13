6a1ffa42ecc78356080b2b6e36295d179e0c1019
rahulbatra-19 <batrarahul409@gmail.com>
2023-10-13 19:55:47 +0530

    Setted up the directory structure

---
+++ app/layout.jsx
@@ -0,0 +1,22 @@
+import "@styles/global.css";
+
+export const metadata = {
+  title: "PromptPal",
+  description: "Discover & Share AI Prompts",
+};
+
+const RootLayout = ({ children }) => {
+  return (
+    <html lang="en">
+      <body>
+        <div className="main">
+          <div className="gradient" />
+        </div>
+
+        <main className="app">{children}</main>
+      </body>
+    </html>
+  );
+};
+
+export default RootLayout;
