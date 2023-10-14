0fcf62e3f5ceb504260212952ccbe0db8992049e
rahulbatra-19 <batrarahul409@gmail.com>
2023-10-14 14:54:00 +0530

    created a prompt

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -9,32 +9,34 @@ const handler = NextAuth({
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
   ],
-  async session({ session }) {
-    const sessionUser = await User.findOne({
-      email: session.user.email,
-    });
-    session.user.id = sessionUser._id.toString();
-    return session;
-  },
-  async signIn({ profile }) {
-    try {
-      // serverles --> Lambda -> dynamodb
-      await connectToDB();
-      // check if a user already exists
-      const userExists = await User.findOne({ email: profile.email });
-      if (!userExists) {
-        await User.create({
-          email: profile.email,
-          username: profile.name.replace(" ", "").toLowerCase(),
-          image: profile.picture,
-        });
+  callbacks: {
+    async session({ session }) {
+      const sessionUser = await User.findOne({
+        email: session.user.email,
+      });
+      session.user.id = sessionUser._id.toString();
+      return session;
+    },
+    async signIn({ profile }) {
+      try {
+        // serverles --> Lambda -> dynamodb
+        await connectToDB();
+        // check if a user already exists
+        const userExists = await User.findOne({ email: profile.email });
+        if (!userExists) {
+          await User.create({
+            email: profile.email,
+            username: profile.name.replace(" ", "").toLowerCase(),
+            image: profile.picture,
+          });
+        }
+        // if not, create a new user
+        return true;
+      } catch (error) {
+        console.error(error);
+        return false;
       }
-      // if not, create a new user
-      return true;
-    } catch (error) {
-      console.error(error);
-      return false;
-    }
+    },
   },
 });
 
